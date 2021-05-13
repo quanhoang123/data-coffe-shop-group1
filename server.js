42a625a58f14ce4adb6fc6fa03e91738bb83e0ab
unknown <Administrator@PNVD167.pn.com.vn>
2021-05-13 12:27:10 +0700

    json server

--- server.js
+++ server.js
@@ -6,24 +6,17 @@ const middlewares = jsonServer.defaults()
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 
+
 // Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-  }
-  // Continue to JSON Server router
-  next()
-})
-const POST= process.env.PORT || 8080;
+
+
+const POST = process.env.PORT || 8080;
 // Use default router
-server.use("/api",router)
+server.use("/api", router)
 server.listen(POST, () => {
-  console.log('JSON Server is running')
+    console.log('JSON Server is running')
 })
